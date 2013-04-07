4ce5bbf8c7cbf361188c44d10257c73ddd326cad
Eric Evenchick <eric@evenchick.com>
2013-04-07 13:45:37 -0400

    added comments

--- src/main.c
+++ src/main.c
@@ -1,20 +1,26 @@
 #include "stm32l1xx.h"
 
 int main() {
-	GPIO_InitTypeDef ledInit;
-	long i = 0;
+    GPIO_InitTypeDef ledInit;
+    long i = 0;
 
-  	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
+    // enable the GPIOB peripheral
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
 
-	ledInit.GPIO_Pin = GPIO_Pin_7 | GPIO_Pin_6;
-	ledInit.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_Init(GPIOB, &ledInit);	
-	GPIO_SetBits(GPIOB, GPIO_Pin_7 | GPIO_Pin_6);
+    // configure pins 6 and 7 as GPIO output
+    ledInit.GPIO_Pin = GPIO_Pin_7 | GPIO_Pin_6;
+    ledInit.GPIO_Mode = GPIO_Mode_OUT;
+    // initialize the peripheral
+    GPIO_Init(GPIOB, &ledInit);
+    // turn pins 6 and 7 on
+    GPIO_SetBits(GPIOB, GPIO_Pin_7 | GPIO_Pin_6);
 
-	for (;;) {
-		GPIO_ToggleBits(GPIOB, GPIO_Pin_7 | GPIO_Pin_6);
-		for (i=0; i<25000; i++);
-	}
-
+    // loop forever
+    for (;;) {
+        // toggle pins 6 and 7
+        GPIO_ToggleBits(GPIOB, GPIO_Pin_7 | GPIO_Pin_6);
+        // waste time
+        for (i=0; i<25000; i++);
+    }
     return 0;
 }
